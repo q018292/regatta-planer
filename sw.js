0eeeca35738d4712f4d5bc2c6d8cd9962a4a27be
q018292 <joerg.benke+GitHub@gmail.com>
2026-08-18 20:53:45 +0200

    v28.0.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='regatta-planer-v28-0-6';
+const CACHE_NAME='regatta-planer-v28-0-7';
 const APP=['./','./index.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
