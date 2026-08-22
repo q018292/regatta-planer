f67fdd1be6901f7969b4bcaea6fda3df820df852
q018292 <joerg.benke+GitHub@gmail.com>
2026-08-22 07:27:57 +0200

    Delete js directory

--- js/importer.js
+++
@@ -1,2 +0,0 @@
-import { readWorkbook } from './data.js';
-export async function importExcel(rakkenFile, bojenFile){ if(!rakkenFile || !bojenFile) throw new Error('Bitte Rakken- und Bojen-Datei auswählen.'); const [rawRakken, rawBojen] = await Promise.all([readWorkbook(rakkenFile), readWorkbook(bojenFile)]); return {rawRakken, rawBojen}; }
