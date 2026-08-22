b8634920003fd68eaa19800e9c6345f22cf8cac4
q018292 <joerg.benke+GitHub@gmail.com>
2026-08-22 09:58:34 +0200

    v30.0.29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='regatta-planer-v30-0-27';
+const CACHE_NAME='regatta-planer-v30-0-29';
 const APP_SHELL=['./','./index.html','./layout.css','./manifest.webmanifest','./sw.js','./icons/icon.svg'];
 
 self.addEventListener('install',event=>{
