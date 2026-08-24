8e88cb04839204d70d0dce8ddb67b53676c2aa20
q018292 <joerg.benke+GitHub@gmail.com>
2026-08-24 19:20:31 +0200

    Update sw.js via RegattaPlaner Uploader

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME='regatta-planer-v30-3-0';
+﻿const CACHE_NAME='regatta-planer-v30-3-1';
 const APP_SHELL=['./','./index.html','./layout.css','./manifest.webmanifest','./sw.js','./icons/icon.svg'];
 
 self.addEventListener('install',event=>{
